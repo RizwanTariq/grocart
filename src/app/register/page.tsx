23f8a6683a1841a8dc1be4f322501fdbb7ba85a9
Rizwan <rizwantariq96@outlook.com>
2025-12-06 22:43:20 +0500

    feat: Refactor RegisterPage to use RegisterContainer for improved structure

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,26 +1,7 @@
-"use client";
-
-import { useState } from "react";
-import RegisterForm from "@/components/RegisterForm";
-import Welcome from "@/components/Welcome";
+import RegisterContainer from "@/components/RegisterContainer";
 
 function RegisterPage() {
-  const [step, setStep] = useState(0);
-  function handleNext() {
-    setStep((prev) => prev + 1);
-  }
-  function handleBack() {
-    setStep((prev) => prev - 1);
-  }
-  return (
-    <div>
-      {step === 0 ? (
-        <Welcome onNext={handleNext} />
-      ) : (
-        <RegisterForm onBack={handleBack} />
-      )}
-    </div>
-  );
+  return <RegisterContainer />;
 }
 
 export default RegisterPage;
---
+++ src/components/RegisterContainer.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import RegisterForm from "@/components/RegisterForm";
+import Welcome from "@/components/Welcome";
+
+function RegisterContainer() {
+  const [step, setStep] = useState(0);
+  function handleNext() {
+    setStep((prev) => prev + 1);
+  }
+  function handleBack() {
+    setStep((prev) => prev - 1);
+  }
+  return (
+    <>
+      {step === 0 ? (
+        <Welcome onNext={handleNext} />
+      ) : (
+        <RegisterForm onBack={handleBack} />
+      )}
+    </>
+  );
+}
+
+export default RegisterContainer;
