639b0d48ea3074bc7d4951dcabe5815250a4318c
Rizwan <rizwantariq96@outlook.com>
2025-12-07 02:16:41 +0500

    feat: Update user model to make password and image fields optional

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,8 +4,9 @@ interface IUser {
   _id?: mongoose.Types.ObjectId;
   name: string;
   email: string;
-  password: string;
+  password?: string;
   contact?: string;
+  image?: string;
   role: "user" | "delivery_boy" | "admin";
 }
 
@@ -13,8 +14,9 @@ export const userSchema = new mongoose.Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: false },
     contact: { type: String, required: false },
+    image: { type: String, required: false },
     role: {
       type: String,
       enum: ["user", "delivery_boy", "admin"],
