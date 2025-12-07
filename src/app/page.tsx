deb632ac049aff270af22e1f53fc6a3cf9388690
Rizwan <rizwantariq96@outlook.com>
2025-12-08 03:11:47 +0500

    feat: Implement user profile check and add EditRoleAndContact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
+import { redirect } from "next/navigation";
+
+import { auth } from "@/auth";
+import EditRoleAndContact from "@/components/EditRoleAndContact";
+import connectDB from "@/libs/db";
+import UserModel from "@/models/user.model";
+
+export default async function Home() {
+  await connectDB();
+  const session = await auth();
+  const user = await UserModel.findOne({ email: session?.user?.email }).lean();
+
+  if (!user) {
+    redirect("/login");
+  }
+  const isIncompleteProfile = !user.contact || !user.role;
+
+  if (isIncompleteProfile) {
+    return <EditRoleAndContact />;
+  }
+
   return <></>;
 }
---
+++ src/components/EditRoleAndContact.tsx
@@ -0,0 +1,7 @@
+function EditRoleAndContact() {
+  return (
+    <div className="flex flex-col min-h-screen p-6 w-full">RoleAndContact</div>
+  );
+}
+
+export default EditRoleAndContact;
