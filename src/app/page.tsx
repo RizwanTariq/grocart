91a0fe64a7866920a9d982b27644395bd8427f16
Rizwan <rizwantariq96@outlook.com>
2025-12-10 22:59:10 +0500

    feat: Refactor Home component to improve user retrieval and profile handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,23 @@ import { auth } from "@/auth";
 import EditRoleAndContact from "@/components/EditRoleAndContact";
 import connectDB from "@/libs/db";
 import UserModel from "@/models/user.model";
+import NavBar from "@/components/navbar/NavBar";
 
 export default async function Home() {
   await connectDB();
   const session = await auth();
-  const user = await UserModel.findOne({ email: session?.user?.email }).lean();
+  const dbUser = await UserModel.findOne({
+    email: session?.user?.email,
+  }).lean();
 
-  if (!user) {
+  if (!dbUser) {
     redirect("/login");
   }
+  const user = JSON.parse(JSON.stringify(dbUser));
   const isIncompleteProfile = !user.contact || !user.role;
 
   if (isIncompleteProfile) {
-    return <EditRoleAndContact />;
+    return <EditRoleAndContact user={user} />;
   }
-
-  return <></>;
+  return <NavBar user={user} />;
 }
