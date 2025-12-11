5247e48e4d241e3846e65211809af007ed338b0e
Rizwan <rizwantariq96@outlook.com>
2025-12-11 18:35:50 +0500

    feat: :sparkles: enabled typed routes and typed envs

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  typedRoutes: true,
   images: {
     remotePatterns: [
       { hostname: "lh3.googleusercontent.com" },
       { hostname: "images.unsplash.com" },
     ],
+  },
+  experimental: {
+    typedEnv: true,
   },
 };
 
