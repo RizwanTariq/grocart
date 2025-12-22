b13a7de914d2a1f0bf1128ce36d7df139fa9229e
Rizwan <rizwantariq95@outlook.com>
2025-12-23 03:41:04 +0500

    feat: :sparkles: Add error handling utilities with prepareErrorResponse and handleGenericError functions

---
+++ src/server/helpers/generic-api-error-handler.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { prepareErrorResponse } from "../errors/prepare-error-response";
+
+function handleGenericError(error: unknown) {
+  if (error instanceof NextResponse) {
+    return error;
+  }
+  console.error(error);
+  return NextResponse.json(
+    prepareErrorResponse(
+      "INTERNAL_SERVER_ERROR",
+      "Unexpected error occurred. Please try again later."
+    ),
+    { status: 500 }
+  );
+}
+export { handleGenericError };
