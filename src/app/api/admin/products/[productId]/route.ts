43876a234f78d36b3eb1acd7173912015d2dc706
Rizwan <rizwantariq95@outlook.com>
2025-12-25 03:53:00 +0500

    feat: Implement product addition and update functionality with error handling

---
+++ src/app/api/admin/products/[productId]/route.ts
@@ -0,0 +1,87 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+import { castIdToObjectId } from "@/server/helpers/mongoose-parser";
+import { uploadOnCloudinary } from "@/libs/cloudinary";
+import connectDB from "@/libs/db";
+import ProductModel from "@/models/products.model";
+import { assertUser } from "@/server/auth/assertUser";
+import { prepareErrorResponse } from "@/server/errors/prepare-error-response";
+import { handleGenericError } from "@/server/helpers/generic-api-error-handler";
+import { convertId } from "@/types";
+import { USER_ROLE } from "@/types/enums";
+
+export const PATCH = auth(async function (request, context) {
+  const params = await context.params;
+  try {
+    const productId = await params.productId;
+    await connectDB();
+    const user = await assertUser(request);
+
+    if (user.role !== USER_ROLE.ADMIN) {
+      throw NextResponse.json(
+        prepareErrorResponse(
+          "UNAUTHORIZED",
+          "Unauthorized: Only admin can update a product"
+        ),
+
+        { status: 401 }
+      );
+    }
+    const formData = await request.formData();
+    const name = formData.get("name")?.toString();
+    const description = formData.get("description")?.toString();
+    const category = formData.get("category")?.toString();
+    const price = Number(formData.get("price")?.toString() || 0);
+    const unit = formData.get("unit")?.toString();
+    const countInStock = Number(formData.get("countInStock")?.toString() || 0);
+
+    const image = formData.get("image") as Blob | null | string;
+
+    if (
+      !name ||
+      !description ||
+      !category ||
+      !price ||
+      !unit ||
+      !countInStock ||
+      !image
+    ) {
+      throw NextResponse.json(
+        prepareErrorResponse("BAD_REQUEST", "Missing required fields"),
+        { status: 400 }
+      );
+    }
+    let imgeUrl;
+    if (typeof image === "string") {
+      imgeUrl = image;
+    } else {
+      imgeUrl = await uploadOnCloudinary(image);
+    }
+
+    const product = await ProductModel.findByIdAndUpdate(
+      castIdToObjectId(productId),
+      {
+        name,
+        description,
+        category,
+        price,
+        unit,
+        countInStock,
+        image: imgeUrl || "",
+      }
+    );
+
+    if (!product) {
+      throw NextResponse.json(
+        prepareErrorResponse("NOT_FOUND", "Product not found"),
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(convertId(product), {
+      status: 201,
+    });
+  } catch (error) {
+    return handleGenericError(error);
+  }
+});
---
+++ src/app/api/admin/products/[productId]/update-stock/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+import { castIdToObjectId } from "@/server/helpers/mongoose-parser";
+import connectDB from "@/libs/db";
+import ProductModel from "@/models/products.model";
+import { assertUser } from "@/server/auth/assertUser";
+import { prepareErrorResponse } from "@/server/errors/prepare-error-response";
+import { handleGenericError } from "@/server/helpers/generic-api-error-handler";
+import { convertId } from "@/types";
+import { USER_ROLE } from "@/types/enums";
+
+export const PATCH = auth(async function (request, context) {
+  const params = await context.params;
+  try {
+    const productId = await params.productId;
+    await connectDB();
+    const user = await assertUser(request);
+
+    if (user.role !== USER_ROLE.ADMIN) {
+      throw NextResponse.json(
+        prepareErrorResponse(
+          "UNAUTHORIZED",
+          "Unauthorized: Only admin can update a product"
+        ),
+
+        { status: 401 }
+      );
+    }
+    const formData = await request.formData();
+
+    const countInStock = Number(formData.get("countInStock")?.toString() || 0);
+
+    if (!countInStock) {
+      throw NextResponse.json(
+        prepareErrorResponse("BAD_REQUEST", "Missing required fields"),
+        { status: 400 }
+      );
+    }
+
+    const product = await ProductModel.findByIdAndUpdate(
+      castIdToObjectId(productId),
+      {
+        countInStock,
+      }
+    );
+
+    if (!product) {
+      throw NextResponse.json(
+        prepareErrorResponse("NOT_FOUND", "Product not found"),
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(convertId(product), {
+      status: 201,
+    });
+  } catch (error) {
+    return handleGenericError(error);
+  }
+});
